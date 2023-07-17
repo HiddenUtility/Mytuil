539fe4791e7a8382ad86822c4e481d1d94a02809
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-07-18 00:13:24 +0900

    bugfix

--- Myutil/UsingPickle.py
+++ Myutil/UsingPickle.py
@@ -5,17 +5,17 @@ Created on Fri Jun  2 16:28:28 2023
 @author: nanik
 """
 
-import pikle
+import pickle
 from pathlib import Path
 class UsingPickle:
     
     @staticmethod
     def load(filepath: Path)-> object:
         with open(filepath, "rb") as f:
-            obj = pikle.load(f)
+            obj = pickle.load(f)
         return obj
     
     @staticmethod
     def dump(filepath: Path, obj: object)-> None:
         with open(filepath, "wb") as f:
-            pikle.dump(obj, f)
+            pickle.dump(obj, f)
