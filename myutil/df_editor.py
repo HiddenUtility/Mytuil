c26dcbace5cb39a78369b72f91386c81645d2be4
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-10-04 22:26:29 +0900

    df_editor create

--- myutil/df_editor.py
+++ myutil/df_editor.py
@@ -6,30 +6,27 @@ Created on Thu Sep 28 05:39:57 2023
 """
 from __future__ import annotations
 from pandas import DataFrame
-
+import unicodedata
 
 class DataFrameEditor:
     def __init__(self, df: DataFrame = DataFrame()):
         if not isinstance(df, DataFrame): raise TypeError("arg is NOT DataFrame")
         self.__df = df
         
-    @property
-    def df(self):
-        return self.__df
-    
     def __chk_column(self, column: str):
-        if column not in set(self.__df.columns): raise NotColumnsContainsError("Not in Column at DataFrame")
+        if column not in set(self.__df.columns): raise NotColumnsContainsError().set_message(column)
     
     def narrow_down_contains(self, column:str, key: str) -> DataFrameEditor:
         if key.upper() == "ALL":return self
         self.__chk_column(column)
         return DataFrameEditor(self.__df[self.__df[column].str.contains(key)])
+    
     def narrow_down_key(self, column:str, key: any) -> DataFrameEditor:
         if key.upper() == "ALL":return self
         self.__chk_column(column)
         return DataFrameEditor(self.__df[self.__df[column]==key])
     
-    def __narrow_query(self, rows: dict, query: str):
+    def __narrow_query(self, rows: dict, query: str) -> dict:
         new_rows = {}
         for i, v in rows.items():
             if query in v:  new_rows[i] = v
@@ -41,15 +38,33 @@ class DataFrameEditor:
             #row : pandas.Serires
             rows[i] = " ".join([str(v) for v in row])
             
-        querys = SearchQuery(search_query).querys
+        querys:set = SearchQuery(search_query).querys
         for query in querys:
             rows = self.__narrow_query(rows, query)
         return DataFrameEditor(self.__df[list(rows.keys())])
+    
+    @property
+    def df(self):
+        return self.__df
+    
 
 class SearchQuery:
-    ...
+    __search_query: str
+    
+    def __init__(self, search_query: str):
+        self.__search_query = self.__normalize(search_query)
+    
+    @staticmethod
+    def __normalize(search_query:str) -> str:
+        return unicodedata.normalize('NFKC', search_query)
+    
+    @property
+    def querys(self) -> set[str]:
+        return set(self.__search_query.split(" "))
     
     
     
 class NotColumnsContainsError(Exception):
-    pass
+    def set_message(self, colmun:str) -> NotColumnsContainsError:
+        message = f"{colmun}はDataFrameに含まれません。"
+        return NotColumnsContainsError(message)
