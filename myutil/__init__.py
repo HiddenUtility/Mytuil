052bc5d2b1d219b007af476d66fc01c8d61d8fbd
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-10-17 23:36:38 +0900

    create thredutil

---
+++ myutil/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 22:52:33 2023
+
+@author: nanik
+"""
+
+from df_editor import DataFrameEditor
+from drive_researcher import DriveResearcher
+from splited_list import SplitedList
---
+++ myutil/splited_list.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 22:55:20 2023
+
+@author: nanik
+"""
+import random
+
+class SplitedList:
+    def __init__(self, list_: list[any] = []):
+        self.__list = list_
+
+    def random_split(self, n: int):
+        if not isinstance(n, int): raise TypeError()
+        length = len(self.__list)
+        if n < 0: raise ValueError()
+        if n > length: raise ValueError()
+        list2 = [list() for _ in range(n)]
+        samples = random.sample(self.__list, length)
+        while True:
+            if len(samples) == 0: break
+            for i in range(n):
+                list2[i].append(samples.pop(0))
+                if len(samples) == 0: break
+        return list2
+            
+        
+
+
+        
+if __name__ == "__main__":
+    
+    list_ = list(range(100))
+    
+    sl = SplitedList(list_)
+    
+    list2 = sl.random_split(4)
+    
+        
