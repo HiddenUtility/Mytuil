0a7e24b4fd30aa4806b914842b0fb1614de6e290
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-12-22 18:06:14 +0900

    fix

--- tkinterutil/main_window.py
+++ tkinterutil/main_window.py
@@ -1,7 +1,13 @@
 import tkinter
 
 class MainWindow(tkinter.Tk):
-    def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
+    def __init__(self, screenName: str | None = None, 
+                 baseName: str | None = None, 
+                 className: str = "Tk", 
+                 useTk: bool = True, 
+                 sync: bool = False, 
+                 use: str | None = None
+                 ) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.__create_freame()
 
