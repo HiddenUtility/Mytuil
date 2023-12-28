47c028d1805b3b9ed9be0c14743be1585e156a80
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-12-28 18:12:34 +0900

    create main window

--- tkinterutil/main_window.py
+++ tkinterutil/main_window.py
@@ -1,6 +1,13 @@
 import tkinter
+from tkinter import Frame
+from tkinterutil.path_insert_frame import PathInsertFrame
+from tkinterutil.date_insert_frame import DateInsertFrame
+from tkinterutil.running_button_frame import RunningButtonFrame
+
 
 class MainWindow(tkinter.Tk):
+    __path_frame : PathInsertFrame
+    __date_frame : DateInsertFrame
     def __init__(self, screenName: str | None = None, 
                  baseName: str | None = None, 
                  className: str = "Tk", 
@@ -9,8 +16,18 @@ class MainWindow(tkinter.Tk):
                  use: str | None = None
                  ) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
+        self.title("MainFrame")
+        self.__main_frame = Frame(self)
         self.__create_freame()
 
     def __create_freame(self):
-        ...
-
+        self.__path_frame = PathInsertFrame(self.__main_frame)
+        self.__date_frame = DateInsertFrame(self.__main_frame)
+        self.__button_frame = RunningButtonFrame(self.__main_frame,
+                                                 self.__path_frame,
+                                                 self.__date_frame,
+                                                 )
+        self.__path_frame.show()
+        self.__date_frame.show()
+        self.__button_frame.show()
+        self.__main_frame.pack(anchor="w")
