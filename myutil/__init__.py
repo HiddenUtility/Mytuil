645e159647d9305f1d7ebf0651f7a22e19a1e902
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2023-12-28 17:42:22 +0900

    rerese

--- myutil/__init__.py
+++ myutil/__init__.py
@@ -2,3 +2,4 @@
 from myutil.df_editor import DataFrameEditor
 from myutil.drive_researcher import DriveResearcher
 from myutil.splited_list import SplitedList
+from myutil.using_pickle import UsingPickle
