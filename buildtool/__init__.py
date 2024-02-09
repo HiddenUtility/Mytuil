ee667d69659732335971023c66591fd704c56be7
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2024-02-09 17:01:06 +0900

    add init

---
+++ buildtool/__init__.py
@@ -0,0 +1,12 @@
+from buildtool.build_tool import BuildTool
+
+__copyright__    = 'Copyright (C) 2024 HiddenUtility'
+__version__      = '1000'
+__license__      = 'BSD-3-Clause'
+__author__       = 'HiddenUtility'
+__author_email__ = 'i.will.be.able.to.see.you@gmail.com'
+__url__          = 'https://github.com/HiddenUtility/pyutil'
+
+__all__ = [
+    'BuildTool',
+    ]
