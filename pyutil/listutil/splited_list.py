3da6d32f44510305e22b6438327549eb2e1ca589
Hiroki Uchimura <i.will.be.able.to.see.you@gmail.com>
2024-03-29 16:51:03 +0900

    fix

--- pyutil/listutil/splited_list.py
+++ pyutil/listutil/splited_list.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-import random
+from random import shuffle
 
 class SplitedList:
     def __init__(self, list_: list[any] = []):
         self.__list = list_
 
-    def random_split(self, n: int):
+    def split(self, n: int, randum = True) -> list[list[any]]:
         if not isinstance(n, int): raise TypeError()
         length = len(self.__list)
         if n < 0: raise ValueError()
-        if n > length: raise ValueError()
+        if n > length: raise ValueError(f'配列の要素 {length} は n = {n}より多くしなければなりません。')
         list2 = [list() for _ in range(n)]
-        samples = random.sample(self.__list, length)
+        samples = self.__list.copy()
+        if randum:
+            shuffle(samples)
         while True:
             if len(samples) == 0: break
             for i in range(n):
@@ -19,10 +21,28 @@ class SplitedList:
                 if len(samples) == 0: break
         return list2
             
-        
-if __name__ == "__main__":
+
+
+def _main():
     list_ = list(range(100))
     sl = SplitedList(list_)
-    list2 = sl.random_split(4)
-    
+    list2 = sl.split(4)
+    [
+        print(l) for l in list2
+    ]
+
+    list_ = list(range(100))
+    sl = SplitedList(list_)
+    list2 = sl.split(4, False)
+    [
+        print(l) for l in list2
+    ]
+   
+
+if __name__ == '__main__':
+    _main()
+
+
+
+
         
